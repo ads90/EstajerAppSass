886bfdc6c0f4acaeca5b90fa2d16cec158262a6d
LaNa <lana.alsabbagh@tatweer.sy>
2025-01-11 12:55:09 +0300

    fix side menu routes . . .

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -8,18 +8,22 @@ import { ProductsApp } from "./components/products/products.component";
 import {ReservationsApp} from './components/reservations/reservations.component';
 import {ReservationDetailsApp} from './components/reservation-details/reservation-details.component';
 import { ProductDetailsComponent } from "./components/product-details/product-details.component";
+import {CalendarApp} from './components/calendar-app/calendar-app.component';
+import {CustomersApp} from './components/customers/customers.component';
 
 export const routes: Routes = [
     { path: '', component: Dashboard, children:[
+        { path: 'login', component: Login },
+        { path: 'signup', component: Signup},
+        { path: 'forgetpassword', component: ForgetPassword},
+        { path: 'overview', component: OverviewApp },
+        { path: 'dashboard', component: Dashboard },
+        { path: 'calender', component: CalendarApp },
+        { path: 'customers', component: CustomersApp },
+        { path: 'products', component: ProductsApp },
+        { path: 'reservations', component: ReservationsApp },
+        { path: 'productDetails/:id', component: ProductDetailsComponent },
         { path: 'reservation-details/:id', component: ReservationDetailsApp },
       ] },
-    { path: 'login', component: Login },
-    { path: 'signup', component: Signup},
-    { path: 'forgetpassword', component: ForgetPassword},
-    { path: 'overview', component: OverviewApp },
-    { path: 'dashboard', component: Dashboard },
-    { path: 'products', component: ProductsApp },
-    { path: 'reservations', component: ReservationsApp },
-    { path: 'productDetails/:id', component: ProductDetailsComponent },
 
 ];
